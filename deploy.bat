575b1670406bcbe2f9dd696ffc5d12914fee827b
xionghy <xionghuiyong@mysteel.com>
2022-05-10 15:38:59 +0800

    新增命令文件

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,11 @@
 call hexo generate
 
 ::复制并覆盖文件及文件夹
-call xcopy D:\blog\Xionghuiyong.github.io\public D:\blog\public /e
+call xcopy D:\blog\Xionghuiyong.github.io\public D:\blog\public /s/y
 
 call git checkout master
+call git add .
+call git commit -m 'add someFile'
+call git push
+
+call git checkout hexo_md
