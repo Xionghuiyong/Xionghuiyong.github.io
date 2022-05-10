52fd69a2542bfcd7363b99ed68258f9a6489caf7
xionghy <xionghuiyong@mysteel.com>
2022-05-10 17:48:22 +0800

    fixbug

--- deploy.bat
+++ deploy.bat
@@ -9,9 +9,3 @@ call mkdir D:\blog\public
 call xcopy D:\blog\Xionghuiyong.github.io\public D:\blog\public /s/y
 
 call git checkout master
-call xcopy D:\blog\public D:\blog\Xionghuiyong.github.io\public /s/y
-call git add .
-call git commit -m "addSomeFile"
-call git push
-
-call git checkout hexo_md
