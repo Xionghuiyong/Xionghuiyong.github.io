c38f45f4fd0329205d25900838fb8134d2534907
xionghy <xionghuiyong@mysteel.com>
2022-05-10 15:55:10 +0800

    addFiles

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,12 @@
-@echo off
+﻿@echo off
+call del /s/q D:\blog\public
 call hexo generate
-
+call mkdir D:\blog\public
 ::复制并覆盖文件及文件夹
 call xcopy D:\blog\Xionghuiyong.github.io\public D:\blog\public /s/y
 
 call git checkout master
+call xcopy D:\blog\public D:\blog\Xionghuiyong.github.io\public /s/y
 call git add .
 call git commit -m 'add someFile'
 call git push
