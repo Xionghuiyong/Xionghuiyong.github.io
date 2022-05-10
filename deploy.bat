a1fa61db6bd029b4900bc630a600b27c11c8f021
xionghy <xionghuiyong@mysteel.com>
2022-05-10 15:33:06 +0800

    add cmd file

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,7 @@
 @echo off
-hexo generate
+call hexo generate
 
-::被复制的源文件夹
-set sourceDir=D:\blog\Xionghuiyong.github.io\public
-::目标文件夹
-set tarDir=D:\blog\public
- 
 ::复制并覆盖文件及文件夹
-xcopy %sourceDir% %tarDir% /e
+call xcopy D:\blog\Xionghuiyong.github.io\public D:\blog\public /e
+
+call git checkout master
